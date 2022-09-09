815e3f921236544471b9175e990d9200b526663a
Cristianortiz19 <88666377+Cristianortiz19@users.noreply.github.com>
2022-09-09 08:52:45 -0500

    Add files via upload

---
+++ public-controller/sketch.js
@@ -0,0 +1,49 @@
+const URL = `http://${window.location.hostname}:5050`;
+let socket = io(URL, { path: '/real-time' });
+
+let directions = null;
+
+function setup() {
+    frameRate(16);
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(0);
+    ellipse(windowWidth / 2, windowHeight / 3, 50, 50);
+    ellipse(windowWidth / 2, windowHeight / 1.5, 50, 50);
+    ellipse(windowWidth / 1.5, windowHeight / 2, 50, 50);
+    ellipse(windowWidth / 3, windowHeight / 2, 50, 50);
+}
+
+function mousePressed() {
+    if(pmouseX > (windowWidth / 2) -25 && pmouseX < (windowWidth / 2) + 25 &
+    pmouseY > (windowHeight / 3)-25 && pmouseY < (windowHeight / 3) + 25){
+        directions = 'UP';
+        console.log(directions);
+    }
+
+    if(pmouseX > (windowWidth / 2)-25 && pmouseX < (windowWidth / 2) + 25 &
+    pmouseY > (windowHeight / 1.5)-25 && pmouseY < (windowHeight / 1.5) + 25){
+        directions = 'DOWN';
+        console.log(directions);
+    }
+
+    if(pmouseX > (windowWidth / 1.5)-25 && pmouseX < (windowWidth / 1.5) + 25 &
+    pmouseY > (windowHeight / 2)-25 && pmouseY < (windowHeight / 2) + 25){
+        directions = 'RIGHT';
+        console.log(directions);
+    }
+
+    if(pmouseX > (windowWidth / 35)-25 && pmouseX < (windowWidth / 3) + 25 &
+    pmouseY > (windowHeight / 2)-25 && pmouseY < (windowHeight / 2) + 25){
+        directions = 'LEFT';
+        console.log(directions);
+    }
+}
+
+/*___________________________________________
+
+1) Create a function that includes the socket method to emit the directions
+_____________________________________________ */
+socket.emit('controller-data', {directions});
---
+++ public-display/sketch.js
@@ -0,0 +1,87 @@
+const URL = `http://${window.location.hostname}:5050`;
+let socket = io(URL, { path: '/real-time' });
+
+let direction = null;
+
+let score = 0;
+let character = {
+    x: 0,
+    y: 0
+};
+let whiteMouse = {
+    x: 50,
+    y: 50
+};
+let speed = 10;
+
+function setup() {
+    frameRate(60);
+    createCanvas(windowWidth, windowHeight);
+    character.x = windowWidth / 2;
+    character.y = windowHeight / 2;
+}
+
+function draw() {
+    background(0, 50);
+    textSize(64);
+    text('🐍', character.x - 25, character.y);
+    textSize(24);
+    text('🐁', whiteMouse.x, whiteMouse.y);
+    eatsMouse();
+    if(direction != null){
+        switch (direction) {
+            case 'UP':
+                character.y -=50;
+                break;
+            case 'DOWN':
+                character.y +=50;
+                break;
+            case 'RIGHT':
+                character.y +=50;
+                break;
+            case 'LEFT':
+                character.y -=50;
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
+
+
+function eatsMouse() {
+    if (dist(character.x, character.y, whiteMouse.x, whiteMouse.y) < 50) {
+        putMouseRandomPosition();
+        score += 10;
+    }
+}
+
+function putMouseRandomPosition() {
+    whiteMouse.x = random(50, windowWidth - 50);
+    whiteMouse.y = random(50, windowHeight - 50);
+}
+
+/*___________________________________________
+
+1) Include the socket method to listen to events and change the character position.
+You may want to use a Switch structure to listen for up, down, right and left cases.
+_____________________________________________ */
+
+socket.on('display-data', data => {
+    let { controllerDirections } = data;
+    direction = controllerDirections;
+    console.log(data);
+})
+
+/*___________________________________________
+
+2) Include the fetch method to post each time the snake eats a mouse
+_____________________________________________ */
+
+fetch(URL+'/Score', {
+    methot: 'POST',
+    body: score
+})
+.then(response => response.json())
+.catch(error => console.error('Error', error))
